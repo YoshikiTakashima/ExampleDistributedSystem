8b8a3601bbfe2dc7cd8216fce1869bda883f6fb2
Yoshiki Takashima <ytakashi@andrew.cmu.edu>
2020-04-05 16:06:11 -0400

    Doubled worker sleep time to prevent message hogging.

--- worker/worker.py
+++ worker/worker.py
@@ -16,7 +16,7 @@ def work_loop(name):
         out_socket.send_string("NODE: {}, ID: {}, DATE: {}".format(name, \
                                                                    inbound, \
                                                                    str(dateTimeObj)))
-        sleep(1)
+        sleep(2)
 
 from sys import argv
 work_loop(argv[1])
